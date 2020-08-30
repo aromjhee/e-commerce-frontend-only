9d9ca04e8c6fc8299caa4c8c8cb5a32bccb7188f
Arom Jhee <aromjhee@gmail.com>
2020-08-30 15:14:22 -0700

    change all 'class' to 'className' in Login.js

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -50,9 +50,9 @@ const LogIn = props => {
             }
           </div>
           <div className='mb-4'>
-            <label htmlFor='email' class='block text-gray-700 text-sm font-bold mb-2'>Email</label>
+            <label htmlFor='email' className='block text-gray-700 text-sm font-bold mb-2'>Email</label>
             <input
-              class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
               placeholder='Enter Email' 
               type='email' 
               name='email' 
@@ -61,9 +61,9 @@ const LogIn = props => {
               onChange={e => setEmail(e.target.value)} />
           </div>
           <div className='mb-6'>
-            <label htmlFor='password' class='block text-gray-700 text-sm font-bold mb-2'>Password</label>
+            <label htmlFor='password' className='block text-gray-700 text-sm font-bold mb-2'>Password</label>
             <input
-              class='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
               placeholder='******************'
               type='password'
               name='password'
