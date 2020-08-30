5a98e82dd95d79ff8a5d290596ec3390e49259d7
Arom Jhee <aromjhee@gmail.com>
2020-08-30 15:07:45 -0700

    update all 'class' in ShippingAddress.js to 'className'

--- src/components/ShippingAddress.js
+++ src/components/ShippingAddress.js
@@ -26,7 +26,7 @@ const ShippingAddress = props => {
           <div className='mb-4'>
             <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='address'>Address</label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type='text'
               name='address'
               id='address'
@@ -36,7 +36,7 @@ const ShippingAddress = props => {
           <div className='mb-4'>
             <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='city'>City</label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type='text'
               name='city'
               id='city'
@@ -46,7 +46,7 @@ const ShippingAddress = props => {
           <div className='mb-4'>
             <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='postalCode'>Postal Code</label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type='number'
               name='postalCode'
               id='postalCode'
@@ -56,7 +56,7 @@ const ShippingAddress = props => {
           <div className='mb-4'>
             <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor='country'>Country</label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type='text'
               name='country'
               id='country'
